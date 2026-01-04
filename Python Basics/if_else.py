66070db88aa04596ff039dd8a02e661cf1e732cb
Prathamesh Shakewar <shakewarprathmesh@gmail.com>
2026-01-04 08:01:56 +0530

    if_else
    if_else for three (x,y,z) var

---
+++ Python Basics/if_else.py
@@ -0,0 +1,14 @@
+'''if else for x,y,z'''
+x=2
+y=4
+z=6
+if x>y:
+  if x>z:
+    print("x is greter then z,y")
+  else:
+   print("z is greter then x,y")
+else:
+  if y>z:
+     print("y is greter then z,x")
+  else:
+     print("z is greter then x,y")
